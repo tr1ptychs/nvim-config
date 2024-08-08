8463255c7587f926e4e2d1e19cfe8425765b8c5f
Wyatt <wyatt.furois@gmail.com>
2024-08-08 17:35:29 -0500

    moved utils, changed dashboard, lazy-lock in .gitignore

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,50 @@
+local icons = require('utils.icons')
+return {
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      require('dashboard').setup {
+        -- config
+        theme = 'hyper',
+        config = {
+          week_header = {
+            enable = true,
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = icons.folderOpenNoBg,
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              icon = icons.treeDiagram,
+              icon_hl = '@variable',
+              desc = 'Explorer',
+              group = 'Lablel',
+              action = "lua require('nvim-tree.api').tree.toggle()",
+              key = 'e'
+            },
+            {
+              icon = icons.console,
+              icon_hl = '@variable',
+              desc = 'Terminal',
+              group = 'Lablel',
+              action = "terminal",
+              key = 't'
+            },
+          },
+        },
+      }
+      vim.keymap.set('n', '<leader>db', '<Cmd>Dashboard<Cr>')
+    end,
+  }
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,38 +1,36 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   --lazy = true,
-  build = ":TSUpdate",
-  config = function ()
-   require'nvim-treesitter.configs'.setup {
-    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    ensure_installed = { "cpp", "javascript", "c", "lua", "vim", "vimdoc",
-                       "query", "markdown", "markdown_inline", "python",
-                       "c_sharp", "css"},
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      ensure_installed = { "cpp", "javascript", "c", "lua", "vim", "vimdoc",
+        "query", "markdown", "markdown_inline", "python",
+        "c_sharp", "css" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = false,
 
-  highlight = {
-    enable = true,
+      highlight = {
+        enable = true,
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
 
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = { "markdown" },
-    },
-  }
- 
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = { "markdown" },
+      },
+    }
   end
 }
