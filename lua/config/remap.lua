dd8cdbfbdee66d9026fc76a7f18035365b299db2
Wyatt <wyatt.furois@gmail.com>
2024-08-12 17:26:28 -0500

    remap edits

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -9,7 +9,7 @@ vim.keymap.set('n', '<leader>tc', '<cmd>BufferClose!<CR>')
 -- change focus to file explorer
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
 
--- move selected lines up and down, obsolute from minimove
+-- move selected lines up and down, obsolete from minimove
 --vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 --vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -20,10 +20,12 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- clear highlight for search
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 -- keep cursor in middle on search terms
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
 -- paste without replacing buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
@@ -37,9 +39,23 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- unmap Ex mode
 vim.keymap.set("n", "Q", "<nop>")
+-- split current buffer, navigate split buffers
+vim.keymap.set("n", "<leader>bs", "<C-w>v")
+vim.keymap.set("n", "<leader>bh", "<C-w>h")
+vim.keymap.set("n", "<leader>bl", "<C-w>l")
+
+-- close current split buffer, close other split buffer
+vim.keymap.set("n", "<leader>bq", "<C-w>q")
+vim.keymap.set("n", "<leader>bo", "<C-w>o")
 
 -- format current file
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
+
+-- quickfix
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>fd", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 -- quickfix navigation
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -49,8 +65,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- start replacing current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- make current file x-ecutable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -23,7 +23,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 -- search settings
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
