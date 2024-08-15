f731a9cb311161240bd844b27cacaeb07678adf4
Wyatt <wyatt.furois@gmail.com>
2024-08-14 19:40:11 -0500

    autocmd for autoformat on save

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,8 +1,14 @@
 -- yank highlighting
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = "Highlight when yanking (copying) text",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Autoformat on save",
+  callback = function()
+    vim.lsp.buf.format()
+  end
 })
