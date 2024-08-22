b4906e5215b80c5d593db4f931201cb636184519
tr1ptychs <wyatt.furois@gmail.com>
2024-08-22 14:23:29 -0500

    added dressing, updated maps and tree

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -68,4 +68,3 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- make current file x-ecutable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
