73fee09a49887ac085273a16f89940f6c975c265
tr1ptychs <wyatt.furois@gmail.com>
2024-12-10 09:56:53 -0600

    python indentation fix

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,14 +1,25 @@
 -- yank highlighting
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = "Highlight when yanking (copying) text",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  desc = "Autoformat on save",
-  callback = function()
-    vim.lsp.buf.format()
-  end
+    desc = "Autoformat on save",
+    callback = function()
+        vim.lsp.buf.format()
+    end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "python",
+    callback = function()
+        vim.opt_local.expandtab = false
+        -- display tabs as 2 spaces wide
+        vim.opt.tabstop = 2
+        vim.opt.softtabstop = 2
+        vim.opt.shiftwidth = 2
+    end,
 })
