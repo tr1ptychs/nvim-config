8193adce4de1166742027e8dd2f631184e52ccdc
Wyatt <wyatt.furois@gmail.com>
2025-03-17 12:39:01 -0500

    python spaces -> tabs

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,34 +1,23 @@
 -- yank highlighting
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = "Highlight when yanking (copying) text",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    desc = "Autoformat on save",
-    callback = function()
-        vim.lsp.buf.format()
-    end
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "python",
-    callback = function()
-        vim.opt_local.expandtab = false
-        -- display tabs as 2 spaces wide
-        vim.opt.tabstop = 2
-        vim.opt.softtabstop = 2
-        vim.opt.shiftwidth = 2
-    end,
+  desc = "Autoformat on save",
+  callback = function()
+    vim.lsp.buf.format()
+  end
 })
 
 -- python tabs
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function()
-    vim.opt_local.expandtab = false
+    vim.opt_local.expandtab = true
     vim.opt.tabstop = 2
     vim.opt.softtabstop = 2
     vim.opt.shiftwidth = 2
